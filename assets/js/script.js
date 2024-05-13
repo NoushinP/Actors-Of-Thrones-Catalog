d5eacb76953e23e55791835c65e70c6ad43229c9
laxsonii <lax_sonii@yahoo.com>
2024-05-13 18:25:25 -0500

    render images to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+let responseData= {};
+
+
 const characterMapping = {
     "Sean Bean": "Eddard Ned Stark",
     "Mark Addy": "Robert Baratheon",
@@ -96,30 +99,75 @@ const imageData = [
 
 // Function to create elements for each character and append to the container
 function populateCharacters() {
-    let container = document.getElementById("image-container");
-    imageData.forEach(function(image) {
-        let div = document.createElement("div");
+    const container = document.getElementById("image-container");
+    imageData.forEach(image => {
+        const div = document.createElement("div");
         div.classList.add("item");
 
-        let img = document.createElement("img");
+        const img = document.createElement("img");
         img.src = image.src;
         img.alt = image.name;
 
-        let h4 = document.createElement("h4");
+        const h4 = document.createElement("h4");
 
          // Create an anchor tag for the name
-        let anchor = document.createElement("a");
+        const anchor = document.createElement("a");
         anchor.href = "#" + image.name.toLowerCase(); // Link to an element with the same id as the name
         anchor.textContent = image.name;
 
 
         // Append the anchor tag to the h4 element
         h4.appendChild(anchor);
+
+
+        // Create a star button for adding to favorites
+        const starButton = document.createElement("button");
+        starButton.textContent = "⭐"; // Star emoji
+        starButton.classList.add("star-button");
+
+        // Add click event listener to the star button
+        starButton.addEventListener("click", function(event) {
+            event.stopPropagation(); // Prevent image click event from firing
+            addToFavorites(image);
+        });
+
         div.appendChild(img);
         div.appendChild(h4);
+        div.appendChild(starButton);
         container.appendChild(div);
     });
 }
+
+// Function to display more details about a character when the image is clicked
+function displayCharacterDetails(character) {
+    // Implementation to display character details (e.g., in a modal or a separate section)
+    console.log("Displaying details for:", character.name);
+}
+
+// Function to add a character to the favorites list
+function addToFavorites(character) {
+    // Implementation to add character to favorites list
+    console.log("Adding to favorites:", character.name);
+    document.addEventListener("DOMContentLoaded", function() {
+        populateCharacters();
+        addImageClickEvent();
+    });
+}
+
+
+// Function to add click event listener to all images in the image container
+function addImageClickEvent() {
+    const images = document.querySelectorAll("#image-container img");
+    images.forEach(image => {
+        image.addEventListener("click", function() {
+            displayCharacterDetails(imageData.find(char => char.src === image.src));
+        });
+    });
+}
+
+// Call the function to add click event listener to images
+addImageClickEvent();
+
 
 populateCharacters();
 
@@ -173,7 +221,7 @@ document.addEventListener("DOMContentLoaded", createImageElements);
 
 // Anchoring some images
 const container = document.getElementById("image-container");
-const imageUrls = ["assets/images/Arya Stark.jpg", "assets/images/Bran Stark.jpg", /* add more image URLs */ ];
+const imageUrls =  ["assets/images/Arya Stark.jpg", "assets/images/Bran Stark.jpg", "assets/images/Brienne of Tarth.jpg", "assets/images/Bronn.jpg", "assets/images/Catelyn Stark.jpg", "assets/images/Cersei Lannister.jpg", "assets/images/Daario Naharis.jpg", "assets/images/Daenerys Targaryen.jpg", "assets/images/Davos Seaworth.jpg", "assets/images/Eddard Ned Stark.jpg", "assets/images/Ellaria Sand.jpg", "assets/images/Gendry.jpg", "assets/images/Gilly.jpg", "assets/images/Grey Worm.jpg", "assets/images/High Sparrow.jpg", "assets/images/Jaime Lannister.jpg", "assets/images/Jaqen H ghar.jpg", "assets/images/Jeor Mormont.jpg", "assets/images/Joffrey Baratheon.jpg", "assets/images/Jon Snow.jpg", "assets/images/Jorah Mormont.jpg", "assets/images/khal drogo.jpg", "assets/images/Margaery Tyrell.jpg", "assets/images/Melisandre.jpg", "assets/images/Missandei.jpg", "assets/images/Petyr littlefinger Baelish.jpg", "assets/images/Ramsay Bolton.jpg", "assets/images/Robb Stark.jpg", "assets/images/Robert Baratheon.jpg", "assets/images/Roose Bolton.jpg", "assets/images/Samwell Tarly.jpg", "assets/images/Sandor Clegane.jpg", "assets/images/Sansa Stark.jpg", "assets/images/Shae.jpg", "assets/images/Stannis Baratheon.jpg", "assets/images/Talisa Maegyr.jpg", "assets/images/Theon Greyjoy.jpg", "assets/images/Tommen Baratheon.jpg", "assets/images/Tormund Giantbane.jpg", "assets/images/Tyrion Lannister.jpg", "assets/images/Tywin Lannister.jpg", "assets/images/Varys.jpg", "assets/images/Viserys Targaryen.jpg", "assets/images/Ygritte.jpg"];
 
 imageUrls.forEach(url => {
     // Create anchor element
@@ -187,44 +235,68 @@ imageUrls.forEach(url => {
 
     // Append anchor to container
     container.appendChild(anchor);
+    document.getElementById('image-container').appendChild(anchor);
 });
 
+
+// Function to add button functionality to all images in the image container
+function addButtonFunctionality() {
+    // Get all the image elements in the container
+    const images = document.querySelectorAll("#image-container img");
+
+    // Loop through each image element
+    images.forEach(image => {
+        // Add an event listener to each image element
+        image.addEventListener("click", function() {
+            // Call the getApi function when an image is clicked
+            displayCharacterDetails(imageData.find(char => char.src === image.src));
+            getApi();
+        });
+    });
+}
+
+// Call the function to add button functionality when the DOM content is loaded
+//document.addEventListener("DOMContentLoaded", addButtonFunctionality);
+
+const fetchButton = document.getElementById('fetch-button');
 // Fetching data from API
-function getApi(event) {
-    event.preventDefault();
+
+const newPage = document.getElementById('new-page');
+const pageContent = document.getElementById('page-content');
+
+function getApi() {
+    // event.preventDefault();
+
     const requestUrl = 'https://thronesapi.com/api/v2/Characters';
+
     fetch(requestUrl)
-        .then(function(response) {
-            console.log('response:', response);
+        .then(function (response) {
+            console.log('response:', response)
             return response.json();
         })
-        .then(function(data) {
+        .then(function (data) {
             console.log(data);
-
-            const container = document.getElementById("image-container");
-
-            data.forEach((box, index) => {
-                const createTableRow = document.createElement('div');
-                const tableData = document.createElement('div');
+            responseData = data;
+            const pageContent = document.getElementById('page-content');
+            data.forEach(function (character) {
+                const imageData = document.createElement('div');
                 const link = document.createElement('a');
-
-                link.textContent = box.html_url;
-                link.href = box.html_url;
-
-                tableData.appendChild(link);
-                createTableRow.appendChild(tableData);
-                container.appendChild(createTableRow);
+                link.textContent = character.name; // Assuming name is the property to be displayed
+                link.href = character.html_url; // Assuming html_url is the property containing the URL
+                pageContent.appendChild(link); // Append link to page content
+                pageContent.appendChild(imageData); // Append image data to page content
             });
         });
 }
 
-// Attach click event listener to the element with id 'imageUrl'
-const imageUrl = document.getElementById('imageUrl');
-imageUrl.addEventListener('click', getApi);
+
+// imageUrls.getElementById('img').addEventListener('click', getApi);
+//fetchButton.addEventListener('click', getApi);
+getApi();
 
 
 
-// fetch request gets a list of all the repos for the node.js organization. All data lives here in the link
-const requestUrl = 'https://thronesapi.com/api/v2/Characters'
+
+
 // fetch request gets a list of all the repos for the node.js organization. All data lives here in the link
 // const requestUrl = 'https://thronesapi.com/api/v2/Characters'w
